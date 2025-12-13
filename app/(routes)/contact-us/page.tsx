31a72fc78896f8f7eb18c609b1ee2c262562e714
maheerali121@gmail.com <maheerali121@gmail.com>
2025-12-13 16:59:30 +0500

    Contact us issue resolved

--- app/(routes)/contact-us/page.tsx
+++ app/(routes)/contact-us/page.tsx
@@ -1,6 +1,8 @@
 import Footer from "@/app/components/Footer";
 import GetInTouch from "@/app/components/GetInTouch";
 import Header from "@/app/components/Header";
+import Link from "next/link";
+import { allIcons } from "@/app/helpers/icons";
 
 function ContactUsPage() {
   return (
@@ -16,6 +18,18 @@ function ContactUsPage() {
       <div className="hidden sm:block">
         <Header forContactUs={true}></Header>
       </div>
+
+      {/* Go back to home link - desktop only */}
+      <Link
+        href="/"
+        className="sm:hidden flex  items-center gap-2 absolute top-8 left-8 z-50 text-base text-text-gray font-normal font-poppins hover:opacity-80 transition-opacity"
+      >
+        <div className="w-6 h-6 text-footer-text mt-0.5">
+          {allIcons.chevronLeft(20, 20)}
+        </div>
+        <span>Go back to home</span>
+      </Link>
+
       <div className="z-10 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 sm:w-full">
         <GetInTouch removeBadge></GetInTouch>
       </div>
