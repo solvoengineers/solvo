51335b8cc80a4da66acc98beb8f49d9120c306ec
Muhammad Adnan <adnan89131@gmail.com>
2026-06-27 05:05:23 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/services/finite-element-anlaysis-fea",
+        destination: "/services/finite-element-analysis-fea",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
