6c92e4e22cc21702bd071e945ebd3f3dfcedfb2c
Muhammad Adnan <adnan89131@gmail.com>
2026-06-29 17:24:39 +0500

    Update SocialMediaIcons.tsx
    whats app link updated

--- app/components/SocialMediaIcons.tsx
+++ app/components/SocialMediaIcons.tsx
@@ -11,7 +11,7 @@ const SocialMediaIcons: React.FC<SocialMediaIconsProps> = ({ small }) => {
     (small ? " social-media-small" : "");
   return (
     <div className="flex flex-row items-center gap-3 ">
-      <a
+      
         target="_blank"
         href="https://www.instagram.com/solvo_engineers"
         className={myClass}
@@ -19,16 +19,16 @@ const SocialMediaIcons: React.FC<SocialMediaIconsProps> = ({ small }) => {
         {allIcons.socialMedia.insta(20, 20)}
       </a>
 
-      <a
+      
         target="_blank"
         href="https://www.linkedin.com/company/solvoengineers"
         className={myClass}
       >
         {allIcons.socialMedia.linkedin(20, 20)}
       </a>
-      <a
+      
         target="_blank"
-        href="https://www.instagram.com/solvo_engineers"
+        href="https://wa.me/message/LUWBKIESNLZSP1"
         className={myClass}
       >
         {allIcons.socialMedia.whatsapp(20, 20)}
